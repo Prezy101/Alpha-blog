71c15b713fed403dcde89a5531673c4b7779dcd3
Rails Udemy Courses <p.litchfieldb@gmail.com>
2018-10-10 12:24:36 +0200

    Created flash message, show page and params to display on the show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,9 +10,19 @@ end
 def create
 
   @article = Article.new(article_params)
-  @article.save
-  #redirect_to articles_show(@article)
-  render plain: params[:article].inspect
+  if @article.save
+    flash[:notice] = "Article successfully created"
+    redirect_to article_path(@article)
+
+  else
+    render 'new'
+  end
+
+end
+
+def show
+
+  @article = Article.find(params[:id])
 
 end
 

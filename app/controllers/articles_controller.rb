15cbb3f256060941ec3c68628fc9e592b5b717bf
Rails Udemy Courses <p.litchfieldb@gmail.com>
2018-10-10 17:01:58 +0200

    Destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,72 +1,55 @@
-
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-def index
 
-  @article = Article.all
-
-end
-
-def new
-
-  @article = Article.new
-
-end
-
-def edit
-
-  @article = Article.find(params[:id])
-
-end
-
-def create
-
-  @article = Article.new(article_params)
-  if @article.save
-    flash[:notice] = 'Article successfully created'
-    redirect_to article_path(@article)
-
-  else
-    render 'new'
+  def index
+    @article = Article.all.order(id: :desc)
   end
 
-end
-
-def update
-
-  @article = Article.find(params[:id])
-
-  if @article.update(article_params)
-    flash[:notice] = 'Article successfully updated'
-    redirect_to article_path(@article)
-
-  else
-    render 'edit'
+  def new
+    @article = Article.new
   end
 
-end
-
-def show
-
-  @article = Article.find(params[:id])
-
-end
-
-def destroy
-
-  @article = Article.find(params[:id])
-  @article.destroy
-  flash[:notice] = 'Article successfully deleted'
-  redirect_to articles_path
-
-end
+  def edit; end
 
 
-private
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article successfully created'
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
 
-def article_params
-  params.require(:article).permit(:title, :description)
-end
+  def update
+    if @article.update(article_params)
+      flash[:notice] = 'Article successfully updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def show; end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = 'Article successfully deleted'
+    redirect_to articles_path
+  end
+
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 
 end

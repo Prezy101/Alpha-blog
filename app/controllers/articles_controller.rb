4f852140ea9b1d7fabdf31b8d1cb78fa6b3445bc
Rails Udemy Courses <p.litchfieldb@gmail.com>
2018-10-10 15:33:09 +0200

    Created index,show, updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,22 @@
 
 class ArticlesController < ApplicationController
 
+
+def index
+
+  @article = Article.all
+
+end
+
 def new
 
   @article = Article.new
+
+end
+
+def edit
+
+  @article = Article.find(params[:id])
 
 end
 
@@ -16,6 +29,20 @@ def create
 
   else
     render 'new'
+  end
+
+end
+
+def update
+
+  @article = Article.find(params[:id])
+
+  if @article.update(article_params)
+    flash[:notice] = "Article successfully updated"
+    redirect_to article_path(@article)
+
+  else
+    render 'edit'
   end
 
 end

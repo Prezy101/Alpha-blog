c34a35a61eafe3faefe8eda80ecae34d89ad4914
Rails Udemy Courses <p.litchfieldb@gmail.com>
2018-10-16 11:20:47 +0200

    Create assosiation and use debug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-
+  has_many :articles
+  before_save {self.email = email.downcase}
   validates :username, presence: true, uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 20 }
             VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,4 +9,3 @@ class User < ApplicationRecord
             format: { with: VALID_EMAIL_REGEX }
 end
 
-cd t

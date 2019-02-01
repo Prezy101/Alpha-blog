a834e58b1cd3af3ea339c63a80d542f8950853cf
Rails Udemy Courses <p.litchfieldb@gmail.com>
2019-02-01 16:03:24 +0200

    Testing Intergration

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,18 +1,18 @@
 require 'test_helper'
 
-class CategoriesControllerTest < ActionDispatch::IntegrationTest
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
   test "get new category form and category" do
 
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
-      post categories_path, params: { category: { name: "Prez" } }
+      post categories_path, params: { category: { name: "President" } }
 
     end
 
     assert_template 'categories/index'
-    assert_match "Prez", response.body
+    assert_match "President", response.body
   end
 
 end

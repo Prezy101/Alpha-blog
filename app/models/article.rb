0735afab83708818e7f4b744e6ce56e3d55c12e6
Rails Udemy Courses <p.litchfieldb@gmail.com>
2019-02-07 16:36:37 +0200

    ArticleCategory

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+
   validates :title, presence: true, length: { minimum: 5, maximun: 50 }
   validates :description, presence: true, length: { minimum: 10, maximun: 300 }
   validates :user_id, presence: true
